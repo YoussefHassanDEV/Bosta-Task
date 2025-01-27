cb678a97ddc0838c86336024e5ef5e979176e56d
Youssef Hassan <devyoussefhassan@gmail.com>
2025-01-27 13:14:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="bg-red-950 flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
